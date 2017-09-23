9f890451dfb3fe7fd2c56274afcec0594fad2824
Antonio Willy Malara <antonio.malara@gmail.com>
2017-09-23 16:19:51 +0200

    Empty project commit

---
+++ ModernHAL/Boilerplate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModernHAL
+//
+//  Created by Antonio Malara on 23/09/2017.
+//  Copyright © 2017 Antonio Malara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ModernHAL/Boilerplate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModernHAL
+//
+//  Created by Antonio Malara on 23/09/2017.
+//  Copyright © 2017 Antonio Malara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
