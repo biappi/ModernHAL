30fd5b97489c164da20647dbf0a2a57c3a3376be
Antonio Willy Malara <antonio.malara@gmail.com>
2017-09-24 06:07:31 +0200

    factoring out symbol(for word:), updateContext(symbol:) and context() into the Model swift wrapper

--- ModernHAL/ModernHAL.swift
+++ ModernHAL/ModernHAL.swift
@@ -24,6 +24,29 @@ class Model {
         initialize_context(wrap)
         wrap.pointee.context.advanced(by: 0).pointee = wrap.pointee.backward
     }
+    
+    func symbol(for word: STRING) -> Int {
+        return Int(find_word(wrap.pointee.dictionary, word))
+    }
+    
+    func updateContext(symbol: Int) {
+        update_context(wrap, Int32(symbol))
+    }
+    
+    func contexts() -> AnyIterator<UnsafeMutablePointer<TREE>?> {
+        var cur = 0
+        let size = Int(self.wrap.pointee.order)
+        
+        return AnyIterator({
+            if cur == size {
+                return nil
+            }
+            else {
+                defer { cur += 1 }
+                return self.wrap.pointee.context.advanced(by: cur).pointee
+            }
+        })
+    }
 }
 
 func modernhal_do_reply(input: String) -> String {
@@ -204,7 +227,7 @@ func modernhal_reply(model: UnsafeMutablePointer<MODEL>,
     return replies
 }
 
-func modernhal_evaluate_reply(model: UnsafeMutablePointer<MODEL>,
+func modernhal_evaluate_reply(model m: UnsafeMutablePointer<MODEL>,
                               keys:  UnsafeMutablePointer<DICTIONARY>,
                               words: UnsafeMutablePointer<DICTIONARY>)
     -> Float32
@@ -216,10 +239,12 @@ func modernhal_evaluate_reply(model: UnsafeMutablePointer<MODEL>,
     var num = 0
     var entropy : Float32 = 0
     
-    Model(wrapping: model).initializeForward()
+    let model = Model(wrapping: m)
+    
+    model.initializeForward()
     
     for i in 0 ..< Int(words.pointee.size) {
-        let symbol = find_word(model.pointee.dictionary, words.pointee.entry.advanced(by: i).pointee)
+        let symbol = model.symbol(for: words.pointee.entry.advanced(by: i).pointee)
         
         if find_word(keys, words.pointee.entry.advanced(by: i).pointee) != 0 {
             var probability : Float32 = 0
@@ -227,12 +252,10 @@ func modernhal_evaluate_reply(model: UnsafeMutablePointer<MODEL>,
             
             num += 1
             
-            for j in 0 ..< Int(model.pointee.order) {
-                if let context = model.pointee.context.advanced(by: j).pointee {
-                    let node = find_symbol(context, Int32(symbol))
-                    probability += Float32(node!.pointee.count) / Float32(context.pointee.usage)
-                    count += 1
-                }
+            for context in model.contexts().flatMap({ $0 }) {
+                let node = find_symbol(context, Int32(symbol))
+                probability += Float32(node!.pointee.count) / Float32(context.pointee.usage)
+                count += 1
             }
             
             if count > 0 {
@@ -240,14 +263,14 @@ func modernhal_evaluate_reply(model: UnsafeMutablePointer<MODEL>,
             }
         }
         
-        update_context(model, Int32(symbol))
+        model.updateContext(symbol: symbol)
     }
     
     
-    Model(wrapping: model).initializeBackward()
+    model.initializeBackward()
     
     for i in (0 ..< Int(words.pointee.size)).reversed() {
-        let symbol = find_word(model.pointee.dictionary, words.pointee.entry.advanced(by: i).pointee)
+        let symbol = model.symbol(for: words.pointee.entry.advanced(by: i).pointee)
         
         if find_word(keys, words.pointee.entry.advanced(by: i).pointee) != 0 {
             var probability : Float = 0
@@ -255,19 +278,18 @@ func modernhal_evaluate_reply(model: UnsafeMutablePointer<MODEL>,
             
             num += 1
             
-            for j in 0 ..< Int(model.pointee.order) {
-                if let context = model.pointee.context.advanced(by: j).pointee {
-                    let node = find_symbol(context, Int32(symbol))
-                    probability += Float(node!.pointee.count) / Float(context.pointee.usage)
-                    count += 1
-                }
+            for context in model.contexts().flatMap({ $0 }) {
+                let node = find_symbol(context, Int32(symbol))
+                probability += Float32(node!.pointee.count) / Float32(context.pointee.usage)
+                count += 1
             }
             
             if count > 0 {
                 entropy -= Float32(log(Double(probability / Float32(count))))
             }
         }
-        update_context(model, Int32(symbol))
+        
+        model.updateContext(symbol: symbol)
     }
     
     if num >= 8 {
